bc5793f7aea134419b8277143b6de075a45f1ebb
David Hochstetler <daviddevs23@gmail.com>
2025-11-18 06:38:33 -0500

    updated stuff

--- static/main.js
+++ static/main.js
@@ -1,12 +1,35 @@
-console.log("tes");
 // web component
 class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+        this.name = 'World';
+    }
   
-  // connect component
-  connectedCallback() {
-    this.textContent = 'Hello World!';
-  }
-  
+    static get observedAttributes() {
+          return ['name'];
+    }
+
+    connectedCallback() {
+      const shadow = this.attachShadow({ mode: 'closed' });
+      shadow.innerHTML = `
+        <style>
+          p {
+            text-align: center;
+            font-weight: normal;
+            padding: 1em;
+            margin: 0 0 2em 0;
+            background-color: #eee;
+            border: 1px solid #666;
+          }
+        </style>
+        <p>Hello ${ this.name }!</p>`;
+
+    }
+    // attribute change
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        this[ property ] = newValue;
+    }
 }
 
 // register component
